9b73187410f912f1051d324895c6c1d2dfbc89c0
Louis Pottier <louspottier44@gmail.com>
2024-08-28 22:01:21 -0300

    Include

---
+++ recipes/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.http import HttpResponse
+from recipes.views import home, contato, sobre
+
+urlpatterns = [
+    path('sobre/', sobre),
+    path('home/', home),
+    path('contato/', contato),
+]
